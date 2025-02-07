4a59dba1c7f3c918971183504dff65314ef1b196
Maria Vitória S. de Melo <vitoriamellomv028@outlook.com>
2025-02-07 17:42:18 -0300

    Revert "ATU"
    This reverts commit 4cd8f04352bfbf24424f954aaa9f2490f02e0ede.

--- demo_frontend/src/main/java/br/edu/ifba/demo/frontend/dto/LivroDTO.java
+++ demo_frontend/src/main/java/br/edu/ifba/demo/frontend/dto/LivroDTO.java
@@ -1,5 +1,6 @@
 package br.edu.ifba.demo.frontend.dto;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -8,12 +9,13 @@ import lombok.Data;
 
 @Data
 public class LivroDTO {
+    
     private Long id_livro;
     private String titulo;
     private String autor;
     private String editora;
     private Integer ano_publicacao;
-    private GeneroDTO genero;
+    private String genero;
     private String isbn;
     private Integer num_paginas;
     private String sinopse;
@@ -21,4 +23,5 @@ public class LivroDTO {
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime data_cadastro;
     private Double preco;
+
 }
